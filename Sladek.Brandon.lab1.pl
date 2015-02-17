ea592a9ef6d0bca5ed940c41a30b823ff48f621d
brandonsladek <brandonsladek@gmail.com>
2015-02-17 16:51:22 -0700

    Added random function to pick between words that have equal frequencies

--- Sladek.Brandon.lab1.pl
+++ Sladek.Brandon.lab1.pl
@@ -98,15 +98,18 @@ while($line = <INFILE>) {
                     if ($word2 ne "a" && $word2 ne "an" && $word2 ne "and" && $word2 ne "by" && $word2 ne "for"
                         && $word2 ne "from" && $word2 ne "in" && $word2 ne "of" && $word2 ne "on" && $word2 ne "or"
                         && $word2 ne "out" && $word2 ne "the" && $word2 ne "to" && $word2 ne "with") {
+                            
+                            # Add bigram to hash of hashes
                             $hoh{$word1}{$word2}++;
                         }
-                }
+                } # End double if loop
             
         } # End while loop
     } # End if loop
     
 } # End while loop, done parsing file
 
+# Print out the number of tracks that made it past the regular expressions
 print "------------------------------------------------------------------\n";
 print "After using regular expressions to filter through the text";
 print " file,\nThe number of valid tracks in $ARGV[0] is: $numTracks\n";
@@ -115,8 +118,7 @@ print "------------------------------------------------------------------\n";
 # Close the file handle
 close INFILE; 
 
-# At this point (hopefully) you will have finished processing the song 
-# title file and have populated your data structure of bigram counts.
+# The file has now been fully processed and the bigram model built
 print "File parsed. The bigram model has been built.\n\n";
 
 # NOTE to user
@@ -128,20 +130,25 @@ $input = <STDIN>;
 chomp($input);
 print "\n";
 
+# Continously loop until user types q
 while ($input ne "q"){
     
-    # Print everything to standard output
+    # Print everything to standard output, see function details below
     print "---------------------------------------------------------------------------------\n";
     print "Most common word following the word $input: ", mcw($input) ,"\n";
     print "Number of distinct words that follow the word $input: ", num_trailing_words($input), "\n\n";
     print "Most probable song title based on the word $input: \n\n\t", make_song_title($input), "\n\n";
     print "---------------------------------------------------------------------------------\n\n";
+    
+    # Ask user if they would like to know the frequency of every unique word that follows the input word
     print "Would you like the full bigram stats on the word? (Enter yes or no) >>> ";
     $answer = <STDIN>;
     chomp($answer);
     print "\n";
     
     if ($answer eq "yes") {
+        
+        # Print out the frequency of every unique word that follows the input word
         print "---------------------------------------------------------------------------------\n";
         print "Here is a list of the frequency of each unique word that follows $input:\n\n";
         print bigram_stats($input);
@@ -149,12 +156,14 @@ while ($input ne "q"){
         print "\n\n";
     }
     
+    # Prompt user for another input word
     print "Enter a word [Enter 'q' to quit]: >>> ";
 	$input = <STDIN>;
     chomp($input);
     print "\n";
 }
 
+# Good manners
 print "Goodbye.\n";
 
 # USER CONTROL FUNCTIONS HERE....
@@ -170,9 +179,18 @@ sub mcw{
         
     # Check the frequency of each word that follows $word and return word with highest frequency
         foreach $second_word (keys %{ $hoh{$word} } ) {
-        if ($hoh{$word}{$second_word} > $freq) {
-            $mcw = "$second_word";
-            $freq = $hoh{$word}{$second_word};
+            
+            if ($freq != 0 && $hoh{$word}{$second_word} == $freq) {
+                # If two or more words have equal frequencies, randomly pick most common word
+                if (int(rand(10)) > 4) {
+                    $mcw = "$second_word";
+                    $freq = $hoh{$word}{$second_word};
+                }
+            }
+            # If the current second_word has a higher frequency than the last one, change mcw to second_word
+            if ($hoh{$word}{$second_word} > $freq) {
+                $mcw = "$second_word";
+                $freq = $hoh{$word}{$second_word};
         }
       }
     } # End if loop
@@ -180,7 +198,7 @@ sub mcw{
     return $mcw;
 } # End of mcw function
 
-# Return song title
+# Return most probable song title
 sub make_song_title{
     my $next_word = shift;
     my $title = "";
@@ -189,10 +207,15 @@ sub make_song_title{
     
     # Append the next most common word to the string while there is a word
     while ($break == 1 && $next_word =~ m/[a-z]/) {
+        
+        # Use hash to keep track of words already appended to title
         $used_words{$next_word}++;
+        
+        # Append $next_word to title and find next most common word
         $title .= "$next_word ";
         $next_word = mcw($next_word);
         
+        # Check to see if the next word has already been appended earlier, avoid infinite loop
         foreach $word (keys %used_words) {
             if ($next_word eq $word) {
                 $break = 0;
@@ -207,6 +230,8 @@ sub make_song_title{
 sub num_trailing_words{
     my $input_word = shift;
     my $count = 0;
+    
+    # Increment count for each distinct word that follows the input word
     foreach my $second_word (keys %{ $hoh{$input_word} }) {
         $count++;
     }
@@ -216,6 +241,8 @@ sub num_trailing_words{
 # Print the frequency of every unique word that follows the input argument word
 sub bigram_stats{
     my $input_word = shift;
+    
+    # Print out the frequency of every distinct word that follows the input word
     foreach my $second_word (keys %{ $hoh{$input_word} }) {
         print "$second_word : $hoh{$input_word}{$second_word}\n";
     }
